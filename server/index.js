c52df882faa7902877d892ff76ee7c93b89a83bf
ananthik354 <ananthik354@gmail.com>
2026-06-27 14:32:39 +0530

    clear cors log

--- server/index.js
+++ server/index.js
@@ -9,10 +9,10 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 // middleware FIRST
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
-
+// app.use(cors({
+//     origin: "http://localhost:3000"
+// }));
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
